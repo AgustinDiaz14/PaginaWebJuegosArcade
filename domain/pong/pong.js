c7d77fb3ae61b08036d02ea3e394527df9f958a5
agustin <tkdagustin2003@gmail.com>
2020-09-14 12:42:55 -0300

    draw middle line

--- domain/pong/pong.js
+++ domain/pong/pong.js
@@ -1,17 +1,17 @@
 const COMPUTER_SPEED = 6
-
+const framesPerSecond = 60
 const PADLE_THICKNESS = 10
 const PADDLE_HEIGHT = 100
 const initialSpeedX = 5
 const initialSpeedY = 2
-const winningScore = 2
-var canvas;
-var canvasContext;
+const winningScore = 3
+var canvas
+var canvasContext
 var showWinScreen = false
-var ballX = 0;
-var ballSpeddX = initialSpeedX;
-var ballY = 0;
-var ballSpeddY = initialSpeedY;
+var ballX
+var ballSpeddX = initialSpeedX
+var ballY
+var ballSpeddY = initialSpeedY
 
 var paddleLeft = 250
 var paddleRight = 250
@@ -24,10 +24,10 @@ var player2Score = 0
 window.onload = function () {
 	canvas = document.getElementById('game');
 	canvasContext = canvas.getContext('2d');
-	ballX = canvas.width/2
-	ballY = canvas.height/2
+	ballX = canvas.width / 2
+	ballY = canvas.height / 2
 
-	var framesPerSecond = 60
+
 	setInterval(function () {
 		moveEverything()
 		computerMovement()
@@ -40,87 +40,83 @@ window.onload = function () {
 	)
 	canvas.addEventListener(
 		'mousemove',
-		function (evt){
+		function (evt) {
 			var mousePos = calculateMousePosition(evt)
-			paddleRight = mousePos.y-(PADDLE_HEIGHT/2)
+			paddleRight = mousePos.y - (PADDLE_HEIGHT / 2)
 		}
 	)
 
 }
 
-function resetGame(evt){
-	restartPlayerPoints(player1Score, player2Score)
-	resetBall()
+function resetGame(evt) {
+	if (showWinScreen) {
+		restartPlayerPoints()
+		resetBall()
+	}
 	showWinScreen = false
 }
 
-function restarBallPosition(initialPositionX, initialPositionY, finalPositionX, finalPositionY){
-	initialPositionX = finalPositionX
-	initialPositionY = finalPositionY
-}
-function restartPlayerPoints(player1, player2){
-	player1 = 0
-	player2 = 0
+function restartPlayerPoints() {
+	player1Score = 0
+	player2Score = 0
 }
 
-function resetBall(){
-	if(player1Score >= winningScore || player2Score >= winningScore){
-		player1Score = 0
-		player2Score = 0
+function resetBall() {
+	if (player1Score >= winningScore || player2Score >= winningScore) {
 		showWinScreen = true
 	}
-	
+
 	ballSpeddY = initialSpeedY
 
 	ballSpeddX = -ballSpeddX
-	ballX = canvas.width/2
-	ballY = canvas.height/2
+	ballX = canvas.width / 2
+	ballY = canvas.height / 2
 }
 
-function calculateMousePosition(evt){
+function calculateMousePosition(evt) {
 	var rect = canvas.getBoundingClientRect()
 	var root = document.documentElement
 	var mouseX = evt.clientX - rect.left - root.scrollLeft
 	var mouseY = evt.clientY - rect.top - root.scrollTop
 
-	return{
+	return {
 		x: mouseX,
 		y: mouseY
 	}
 }
-function computerMovement(){
-	var paddleLeftCenter = paddleLeft + (PADDLE_HEIGHT/2)
-	if(paddleLeftCenter < ballY-35){
+function computerMovement() {
+	var paddleLeftCenter = paddleLeft + (PADDLE_HEIGHT / 2)
+	if (paddleLeftCenter < ballY - 35) {
 		paddleLeft += COMPUTER_SPEED
 	}
 
-	else if(paddleLeftCenter > ballY+35){
+	else if (paddleLeftCenter > ballY + 35) {
 		paddleLeft -= COMPUTER_SPEED
 	}
-	
+
 }
 function moveEverything() {
-	if(showWinScreen){return}
+	if (showWinScreen) { return }
 	ballX += ballSpeddX
 	ballY += ballSpeddY
 	if (ballX < 0) {
-		if(ballY > paddleLeft && ballY < PADDLE_HEIGHT+paddleLeft){
+		if (ballY > paddleLeft && ballY < PADDLE_HEIGHT + paddleLeft) {
 			ballSpeddX = -ballSpeddX
-			
-			var deltaY = ballY - (paddleLeft + PADDLE_HEIGHT/2)
+
+			var deltaY = ballY - (paddleLeft + PADDLE_HEIGHT / 2)
 			ballSpeddY = deltaY * 0.35
 		}
-		else{
+		else {
 			player2Score++
 			resetBall()
 		}
-		
+
 	}
 	if (ballX > canvas.width) {
-		if(ballY > paddleRight	 && ballY < PADDLE_HEIGHT+paddleRight){
+		if (ballY > paddleRight && ballY < PADDLE_HEIGHT + paddleRight) {
 			ballSpeddX = -ballSpeddX
 		}
-		else{
+		else {
 			player1Score++
 			resetBall()
 		}
@@ -134,31 +130,42 @@ function moveEverything() {
 	}
 }
 
-function drawRectangle(color, positionX, positionY, width, height){
+function drawRectangle(color, positionX, positionY, width, height) {
 	canvasContext.fillStyle = color
 	canvasContext.fillRect(positionX, positionY, width, height)
 }
 
 function drawEverything() {
-	if(showWinScreen){
+	if (showWinScreen) {
+		drawRectangle("black", 0, 0, canvas.width, canvas.height)
+		if(player1Score >= winningScore){
+			canvasContext.fillStyle = "white";
+			canvasContext.fillText("Player 1 won!", canvas.width/2, canvas.height/2)	
+		}
+
+		else if(player2Score >= winningScore){
+			canvasContext.fillStyle = "white";
+			canvasContext.fillText("Player 2 won!", canvas.width/2, canvas.height/2)
+		}
 		canvasContext.fillStyle = "white";
-		canvasContext.fillText("Click to restart", canvas.width/2-20, canvas.height/2-100)
+		canvasContext.fillText("Click to restart", canvas.width / 2, canvas.height / 2 - 100)
 		return
 	}
-	
+
 	drawRectangle("black", 0, 0, canvas.width, canvas.height)
 
-	
+	drawRectangle("white", canvas.width/2, 0, 2, canvas.height)
+
 	drawRectangle("white", 0, paddleLeft, PADLE_THICKNESS, 100)
 
-	
-	drawRectangle("white", canvas.width-PADLE_THICKNESS, paddleRight, PADLE_THICKNESS, 100)
+	drawRectangle("white", canvas.width - PADLE_THICKNESS, paddleRight, PADLE_THICKNESS, 100)
 
 	canvasContext.fillStyle = "white";
 	canvasContext.beginPath()
 	canvasContext.arc(ballX, ballY, 10, 2 * Math.PI, false)
 	canvasContext.fill()
 
-	canvasContext.fillText(player1Score, canvas.width/2-100, canvas.height/2)
-	canvasContext.fillText(player2Score, canvas.width/2+100, canvas.height/2)
+
+	canvasContext.fillText(player1Score, canvas.width / 2 - 100, canvas.height / 2)
+	canvasContext.fillText(player2Score, canvas.width / 2 + 100, canvas.height / 2)
 }
