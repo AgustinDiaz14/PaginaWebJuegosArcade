173a27656d3633dcbd3fc8acd49fdd99c14f9c2c
federico <atadiafederico@gmail.com>
2020-09-29 19:55:24 -0300

    test firebase

---
+++ data/login.js
@@ -0,0 +1,6 @@
+auth.createUsernWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
