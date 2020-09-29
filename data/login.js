4ecc86e6c1ad14703786cabbd02fbaf3fc70d111
agustin <tkdagustin2003@gmail.com>
2020-09-29 20:12:27 -0300

    correction of firebase bugs

--- data/login.js
+++ data/login.js
@@ -1,4 +1,7 @@
-auth.createUsernWithEmailAndPassword(email, password).catch(function(error) {
+const email=document.querySelector("#emailField")
+const password=document.querySelector("#passwordField")
+
+auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
