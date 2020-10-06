73db37b0df6dcab3f51be3bc4b106d960210ba53
agustin <tkdagustin2003@gmail.com>
2020-10-06 17:59:01 -0300

    create file of firebase configurations

---
+++ data/firebaseConf.js
@@ -0,0 +1,13 @@
+
+var firebaseConfig = {
+			apiKey: "AIzaSyBTTMvFRHFHDAAkEJt0__9KpkIIjEoJ1Sw",
+			authDomain: "paginawebjuegosarcade.firebaseapp.com",
+			databaseURL: "https://paginawebjuegosarcade.firebaseio.com",
+			projectId: "paginawebjuegosarcade",
+			storageBucket: "paginawebjuegosarcade.appspot.com",
+			messagingSenderId: "627296857083",
+			appId: "1:627296857083:web:98de3be8556ce8d38d795e"
+		};
+
+		firebase.initializeApp(firebaseConfig);
+		const auth = firebase.auth()
--- data/login.js
+++ data/login.js
@@ -1,3 +1,4 @@
+
 function login(){
   var email= document.getElementById("emailField").value
   
