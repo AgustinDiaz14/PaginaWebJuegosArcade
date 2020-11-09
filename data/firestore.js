3947e2f19c70658e6d72f67baa7d4ca6cf6d0a5a
agustin <tkdagustin2003@gmail.com>
2020-11-09 11:18:20 -0300

    give error message when creating user and change the way that points of snake are saved

--- data/firestore.js
+++ data/firestore.js
@@ -1,7 +1,26 @@
 
 function saveToFirestore(points){
-    firestore.collection(auth.currentUser.uid).add({snakePoints: points})
-    .then(function(){
-        //window.alert(points)
+    let user = auth.currentUser.uid
+    let documents = firestore.collection("snakePoints").doc(user)
+    documents.get()
+    .then(function(doc){
+        console.log(doc.data().snakePoints)
+        if(doc.exists){
+            if(doc.data().snakePoints < points){
+
+            console.log("enter if")
+            documents.delete().then(function(){
+                console.log("deleted")
+            })
+
+            documents.set({snakePoints: points})
+            }
+        }
+
+        else{
+            documents.set({snakePoints: points})
+        }
+        
     })
+    
 }
--- data/singUp.js
+++ data/singUp.js
@@ -1,13 +1,44 @@
 function signUp(){
     var email= document.getElementById("email").value
-    
+    var name = document.getElementById("user").value
     var password= document.getElementById("password").value
-    
+    var confirmPassword = document.getElementById("confirmPassword").value
+    console.log(email)
+    if(email == [] || name == [] || password == [] || confirmPassword == []){
+        window.alert("Por favor complete los campos")
+        return
+    }
+
+    if(password != confirmPassword){
+        window.alert("Las contraseñas deben ser iguales, por favor intente nuevamente")
+        return
+    }
   
     auth
     .createUserWithEmailAndPassword(email, password)
-    .then(user => {
+    .then(result =>{
+        auth.currentUser.updateProfile({
+            displayName: name
+        })
         window.location.href = './index.html'
+    })
+    .catch(function(error){
+        switch(error.message){
+            case "The email address is badly formatted.":{
+                window.alert("Email inválido"); break
+            }
+            case "Password should be at least 6 characters":{
+                window.alert("La contraseña debe tener al menos 6 caracteres"); break
+            }
+            case "The email address is already in use by another account.":{
+                window.alert("El usuario ya existe"); break
+            }
+
+            default:{
+                console.log(error.message)
+                window.alert("A ocurrido un error, por favor intente nuevamente más tarde"); break
+            }
+        }
     })
     
   }
